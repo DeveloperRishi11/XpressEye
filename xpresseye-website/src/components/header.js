7b1eada73ba6f6555a87b79b56f979c04c4d0d5e
Rishi Sharma <developersharmarishi@gmail.com>
2025-08-02 16:07:40 +0000

    added structure

---
+++ xpresseye-website/src/components/header.js
@@ -0,0 +1,14 @@
+export function createHeader() {
+  return `
+    <header>
+      <nav class="container">
+        <div class="logo">XpressEye</div>
+        <ul class="nav-links">
+          <li><a href="#home">Home</a></li>
+          <li><a href="#services">Services</a></li>
+          <li><a href="#contact">Contact</a></li>
+        </ul>
+      </nav>
+    </header>
+  `
+}
